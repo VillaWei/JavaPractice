041fa0bc7707b57cb086d6f2fa2ac142e2223a3a
138****7510 <villa_juve@hotmail.com>
2023-02-06 08:46:22 +0000

    Update Typo

--- src/main/java/com/Concurrent_Java/Test_Thread_Safety.java
+++ src/main/java/com/Concurrent_Java/Test_Thread_Safety.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
-public class Test_Thread_Safety {
+public class Test_Thread_Safety{
     @NotThreadSafe
     public static class UnsafeThread {
         private int value;
