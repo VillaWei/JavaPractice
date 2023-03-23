b3da1416256fe46214072fa2ea065707e611b69d
VillaWei <124343784+VillaWei@users.noreply.github.com>
2023-03-23 08:12:18 +0800

    Update finding

--- src/main/Task_006/TimeOutCancelTest.java
+++ src/main/Task_006/TimeOutCancelTest.java
@@ -6,15 +6,12 @@ import java.util.concurrent.TimeoutException;
 
 public class TimeOutCancelTest {
      public static void main(String[] args) throws BrokenBarrierException, InterruptedException, TimeoutException {
-        final int THREAD_COUNT = 5;
-        final int TIMEOUT_IN_SECOND = 5;
-        final int PROCESS_TIME = 10;
 
         ImprovedTestHarness improvedTestHarness = new ImprovedTestHarness();
 
-        System.out.println(String.format("The execution time:[%d]", improvedTestHarness.timeTasks(THREAD_COUNT, TIMEOUT_IN_SECOND, ()->{
+        System.out.println(String.format("The execution time:[%d]", improvedTestHarness.timeTasks(5, 5, ()->{
             try{
-                TimeUnit.SECONDS.sleep(PROCESS_TIME);
+                TimeUnit.SECONDS.sleep(10);
             }catch(InterruptedException interruptedException){
                 System.out.println(Thread.currentThread().getName() + " has been cancelled.");
             }
